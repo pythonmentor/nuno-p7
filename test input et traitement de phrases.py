187e322f18c0158ce89612d6350d83682a6d6ff9
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-10-12 00:20:29 +0200

    cotrection phrase

--- test input et traitement de phrases.py
+++ test input et traitement de phrases.py
@@ -1,21 +1,20 @@
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
- 
-print("Salut, je suis GrandPy, je suyis là afion de t'aider ;-)")
+nltk.download('stopwords')
+nltk.download('punkt')
 
-user_input = input('Que veux-tu savoir mon enfant?')
+print("Salut, je suis GrandPy, je suis là afin de t'aider ;-)")
+
+user_input = input('Que veux-tu savoir mon enfant?  :')
 
 stop_words = set(stopwords.words('french'))
-words = word_tokenize(user_input, language = 'french')
+words = word_tokenize(user_input, language='french')
 
 new_sentence = []
- 
 for word in words:
     if word not in stop_words:
-        new_sentence.append(word)
- 
+        if len(word) > 2:
+            new_sentence.append(word)
 print(new_sentence)
-
+position_words = ["lieu", "adresse", "trouve", "situe"]
