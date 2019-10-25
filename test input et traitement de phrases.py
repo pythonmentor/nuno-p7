b15751a227eb84ca970d068e1e6efe9a5544734b
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-10-25 23:00:39 +0200

    parseur avancée Ntkl viré

--- test input et traitement de phrases.py
+++ test input et traitement de phrases.py
@@ -1,8 +1,5 @@
 import re
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('punkt')
+import json
 
 print("Salut, je suis GrandPy, je suis là afin de t'aider ;-)")
 
@@ -11,32 +8,22 @@ def get_user_choice():
     """
     Get and parse user's sentences.
     """
+
     while True:
-        
         user_input = input('Que veux-tu savoir mon enfant?  :  ')
         user_input = re.split(",|'| |-", user_input)
-        stop_words = set(stopwords.words('french'))
-        stop_words_capitalize = [x.capitalize() for x in stop_words]
-
-        new_stop_words = list(stop_words) 
-        words_to_eliminate = new_stop_words + stop_words_capitalize
-
+        user_input = [x.lower() for x in user_input]
+    
+        with open('stop_words.json') as json_stop_words:
+            stop_words_custom = json.load(json_stop_words)
+        
         new_sentence = []
+
         for word in user_input:
-            if word not in words_to_eliminate:
+            if word not in stop_words_custom:
                 if len(word) > 2:
                     if word[-2:] != "er":
-                        if word[0].isupper():
-                            new_sentence.append(word)
-        print(new_sentence)
-
-        if len(new_sentence) == 0:
-
-            print("Tu sais, avec mon age, " +
-                "je ne t'ai pas trés bien compris mon enfant, " +
-                "peut-tu me rappeller en utilisant une majuscule, " +
-                "afin que je puisse mieux comprendre!  ")
-            continue
+                        new_sentence.append(word)
 
         if len(new_sentence) >= 1:
             print("Mots a envoyer", new_sentence)
@@ -45,6 +32,6 @@ def get_user_choice():
         else:
             break
 
-        
+
 if __name__ == "__main__":
     get_user_choice()
