41a4a387e565becf3ef0f921add24275d32a818e
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-10-27 18:07:30 +0100

    premiére ebauche  body program

--- GrandPy/static/scripts/body.py
+++ GrandPy/static/scripts/body.py
@@ -1,19 +1,25 @@
 from .first_input_parser import parse_user_input
 
 
-print("Salut, je suis GrandPy, je suis là afin de t'aider ;-)")
+class UserInterface:
+    """
+    User program interface and body of program
+    """
 
+    print("Salut, je suis GrandPy, je suis là afin de t'aider ;-)")
 
-while True:
-    user_input = input('Que veux-tu savoir mon enfant?  :  ')
-    parsed_user_input = parse_user_input(user_input)
+    def user_first_question():
+        while True:
+            user_input = input('Que veux-tu savoir mon enfant?  :  ')
+            parsed_user_input = parse_user_input(user_input)
 
-    if len(parsed_user_input) >= 1:
-        print("Mots a envoyer", parsed_user_input)
-        break
-    else:
-        continue
-
+            if len(parsed_user_input) >= 1:
+                print("Mots a envoyer", parsed_user_input)
+                break
+            else:
+                continue
+        return parsed_user_input
+        
 
 if __name__ == "__main__":
     pass
--- GrandPy/static/scripts/first_input_parser.py
+++ GrandPy/static/scripts/first_input_parser.py
@@ -9,10 +9,9 @@ def parse_user_input(user_input):
 
     user_input = re.split(",|'| |-", user_input)
     user_input = [x.lower() for x in user_input]
-
     with open('stop_words.json') as json_stop_words:
         stop_words_custom = json.load(json_stop_words)
-        
+    
     new_sentence = []
 
     for word in user_input:
