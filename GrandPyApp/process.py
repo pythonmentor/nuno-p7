a566cb10ba9e1b9854621516394cde9c6ee40ecb
NunoMars <nuno.ricardo.mars@gmail.com>
2020-01-20 22:28:08 +0100

    essai key error

--- GrandPyApp/process.py
+++ GrandPyApp/process.py
@@ -26,7 +26,7 @@ def grandPyWork(message, app):
         )
     try:
         location = msg_gmaps["results"][0]["geometry"]["location"]
-    except IndexError:
+    except KeyError:
         ups = {"messages": [
             "Desolé je n'ai pas pu t'aider mon petit...",
             "Écris mieux tête de linotte!",
