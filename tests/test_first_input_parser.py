55ff03bec796ef4febb3c86505d3182c0cdc1469
NunoMars <nuno.ricardo.mars@gmail.com>
2020-08-17 11:27:44 +0200

    change tests

--- tests/test_first_input_parser.py
+++ tests/test_first_input_parser.py
@@ -1,4 +1,4 @@
-from GrandPyApp import first_input_parser as first
+from GrandPyApp.first_input_parser import parse_user_input as parse
 from .config import STOP_WORDS
 
 
@@ -8,12 +8,12 @@ stop_words_custom = STOP_WORDS
 def test_parse_user_input():
     test_user_input = "Bonjour GrandPy peut-tu me donner"\
         " l'adresse d'openclassrooms stp?"
-    assert first.parse_user_input(
+    assert parse(
         test_user_input,
         stop_words_custom) == "openclassrooms"
 
 
 def test_parse_user_input_empty():
     test_empty_user_input = " "
-    assert first.parse_user_input(
+    assert parse(
         test_empty_user_input, stop_words_custom) == []
